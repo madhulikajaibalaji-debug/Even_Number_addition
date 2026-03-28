97874e1c5977eadcd65fb021ae6b9e3f400b941b
Madhulika <madhulikajaibalaji@gmail.com>
2026-03-28 16:33:20 +0530

    Updated

--- add.py
+++ add.py
@@ -3,7 +3,7 @@ numbers = [10, 15, 22, 33, 40, 55, 60]
 sum_even = 0
 
 for num in numbers:
-    if num % 2 == 0:
+    if num % 2 != 0:
         sum_even += num
 
 print("Sum of even numbers:", sum_even)
